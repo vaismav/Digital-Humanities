f2e5114c5e803b7d8685f794738d4c40e5a126d5
Avishai Vaisman <aviciiv@gmail.com>
2020-11-01 01:52:31 +0200

    exporting the data to text files in output directory

--- Get all the Laws of Israel/script.py
+++ Get all the Laws of Israel/script.py
@@ -1,29 +1,56 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-import csv
+import os
+from datetime import datetime
+
 
 PATHtoChromeDtiver = "./chromedriver"
 
 driver=webdriver.Chrome(PATHtoChromeDtiver)
 
 def exportLineToFile(file, outputStr):
-    print(outputStr)
+    file.write(outputStr + "\n")
 
 
-url="https://main.knesset.gov.il/Activity/Legislation/Laws/Pages/LawLaws.aspx?t=lawlaws&st=lawlaws&pn=2&sb=LatestPublicationDate&so=D"
+def exportLawsList(url, fileName):
+    print(url)
+    filePath = os.getcwd() + "/output/" + datetime.today().strftime('%Y-%m-%d-%H:%M:%S') + fileName + ".txt"
+    file = open( filePath , "a")
+    
+    pageId=1
+    searchNextPage = True
+    
+    searchURL = url + str(pageId)
 
-driver.get(url)
+    print(searchURL)
+    print(type(searchURL))
+    driver.get(searchURL)
+    
+    tableHeaders = driver.find_elements_by_class_name("rgHeader.LawListHeaderRow")
+    outputStr = ""
+    for header in tableHeaders:
+        outputStr += header.text + ','
+    exportLineToFile(file, outputStr)
+    
+    
 
-tableHeaders = driver.find_elements_by_class_name("rgHeader.LawListHeaderRow")
-outputStr = ""
-for header in tableHeaders:
-    outputStr += header.text + ','
-exportLineToFile(None, outputStr)
+    while searchNextPage:
+        searchURL = url + str(pageId)
+        driver.get(searchURL)
+        elements = driver.find_elements_by_class_name("rgRow")
+        if len(elements) == 0:
+            searchNextPage = False
+        else:
+            for element in elements:
+                attributesList = element.find_elements(By.TAG_NAME, "td")
+                outputString = ""
+                for attribute in attributesList:
+                    outputString += attribute.text+ ','
+                exportLineToFile(file,outputString)
+        pageId += 1
 
-elements = driver.find_elements_by_class_name("rgRow")
-for element in elements:
-    attributesList = element.find_elements(By.TAG_NAME, "td")
-    outputString = ""
-    for attribute in attributesList:
-       outputString += attribute.text+ ','
-    print(outputString)
+    file.close()
+
+
+
+exportLawsList("https://main.knesset.gov.il/Activity/Legislation/Laws/Pages/LawLaws.aspx?t=lawlaws&st=lawlaws&pn=", "LawLaws")
