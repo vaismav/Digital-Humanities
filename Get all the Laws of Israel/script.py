5753a1eef3bbd60ddb47b24e020a7495151b9c75
Avishai Vaisman <aviciiv@gmail.com>
2020-10-31 20:55:03 +0200

    extract csv strings for heders and entries. sending strings of lines to the csv file to export function, need to implement export function

--- Get all the Laws of Israel/script.py
+++ Get all the Laws of Israel/script.py
@@ -1,10 +1,29 @@
 from selenium import webdriver
-PATH = "./chromedriver"
+from selenium.webdriver.common.by import By
+import csv
 
-driver=webdriver.Chrome(PATH)
+PATHtoChromeDtiver = "./chromedriver"
+
+driver=webdriver.Chrome(PATHtoChromeDtiver)
+
+def exportLineToFile(file, outputStr):
+    print(outputStr)
+
+
 url="https://main.knesset.gov.il/Activity/Legislation/Laws/Pages/LawLaws.aspx?t=lawlaws&st=lawlaws&pn=2&sb=LatestPublicationDate&so=D"
 
 driver.get(url)
-s=driver.find_elements_by_class_name("rgRow")
-for i in range(len(s)):
-    print(s[i].text)
+
+tableHeaders = driver.find_elements_by_class_name("rgHeader.LawListHeaderRow")
+outputStr = ""
+for header in tableHeaders:
+    outputStr += header.text + ','
+exportLineToFile(None, outputStr)
+
+elements = driver.find_elements_by_class_name("rgRow")
+for element in elements:
+    attributesList = element.find_elements(By.TAG_NAME, "td")
+    outputString = ""
+    for attribute in attributesList:
+       outputString += attribute.text+ ','
+    print(outputString)
